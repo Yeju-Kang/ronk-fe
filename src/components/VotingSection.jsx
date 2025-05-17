66b05d749ba066fa80123c46fd65c4c95c2cd248
Yeju-Kang <bbogaeme@github.com>
2025-05-17 23:53:25 +0900

    fix: VotingSection 타이머를 제품 사이 중앙에 정렬

--- src/components/VotingSection.jsx
+++ src/components/VotingSection.jsx
@@ -4,8 +4,8 @@ import "./VotingSection.css";
 
 const VotingSection = () => (
   <section className="voting-section">
-    <div className="timer">00:00</div>
     <div className="product-card">제품 A</div>
+    <div className="timer">00:00</div>
     <div className="product-card">제품 B</div>
   </section>
 );
